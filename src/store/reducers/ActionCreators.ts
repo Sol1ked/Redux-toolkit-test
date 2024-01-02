98edc7061bbe050dffa5ddb9e1aa955e81852e63
Sol1ked <aprima337228@gmail.com>
2024-01-02 20:16:31 +0700

    начал rtk query

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -2,18 +2,24 @@ import axios from 'axios';
 import { AppDispatch } from '../store';
 import { IUser } from '../../models/IUser';
 import { userSlice } from './UserSlice';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 function getErrorMessage(error: unknown) {
   if (error instanceof Error) return error.message;
   return String(error);
 }
 
-export const fetchUsers = () => async (dispatch: AppDispatch) => {
-  try {
-    dispatch(userSlice.actions.usersFetchingLoading());
-    const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
-    dispatch(userSlice.actions.usersFetchingSuccess(response.data));
-  } catch (e) {
-    dispatch(userSlice.actions.usersFetchingError(getErrorMessage(e)));
-  }
-};
+// export const fetchUsers = () => async (dispatch: AppDispatch) => {
+//   try {
+//     dispatch(userSlice.actions.usersFetchingLoading());
+//     const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+//     dispatch(userSlice.actions.usersFetchingSuccess(response.data));
+//   } catch (e) {
+//     dispatch(userSlice.actions.usersFetchingError(getErrorMessage(e)));
+//   }
+// };
+
+export const fetchUsers = createAsyncThunk('user/fetchAll', async (_, thunkApi) => {
+  const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+  return response.data;
+});
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { IUser } from '../../models/IUser';
+import { fetchUsers } from './ActionCreators';
 
 interface UserState {
   users: IUser[];
@@ -29,6 +30,22 @@ export const userSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+
+      .addCase(fetchUsers.pending, (state) => {})
+
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = '';
+        state.users = action.payload;
+      })
+
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
   },
 });
 
